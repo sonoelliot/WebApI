041d823899a6ae737c4df1e455eff18b2997bef8
Elliot Sitoe <cyber2se@gmail.com>
2019-05-02 10:42:53 +0200

    added the error handling

--- WebApI/Controllers/ValuesController.cs
+++ WebApI/Controllers/ValuesController.cs
@@ -17,7 +17,6 @@ namespace WebApI.Controllers
        
         ApiHelper web = new ApiHelper();
 
-
         /// <summary>
         /// Returns the value closest to zero which is supplied via an array parameter.
         /// </summary>
@@ -25,6 +24,12 @@ namespace WebApI.Controllers
         [Route("ClosestToZero")]
         public int Post([FromBody] int[] intArray)
         {
+
+            if (intArray == null)
+            {
+                throw new NullReferenceException ("IntArray can't be null");
+            }
+
             int nearest = 0;
             try
             {
@@ -33,6 +38,7 @@ namespace WebApI.Controllers
             catch (Exception ex)
             {
                 web.WriteLog(ex, MethodBase.GetCurrentMethod().Name);
+                throw ex;
                 //return Json(ex.Message, JsonRequestBehavior.AllowGet);
             }
             return nearest;
@@ -49,27 +55,39 @@ namespace WebApI.Controllers
 
             int index = 0, sumUsingWhileLoop = 0;
             int TotalForLoop = 0;
-
-            //sum for While Loop
-            while (index < intArray.Count)
+            if (intArray == null)
             {
-                sumUsingWhileLoop += intArray[index];
-                index = index + 1;
+                throw new NullReferenceException("intArray can't be null");
             }
-
-            //sum for ForLoop
-            foreach (var item in intArray)
-            {
-                TotalForLoop = TotalForLoop + item;
-
-            }
-            //sum for recursion
-            int recursion = web.SumRecurssion(intArray, 0, intArray.Count);
-
             var results = new Dictionary<string, int>();
-            results.Add("Sum for WhileLoop", sumUsingWhileLoop);
-            results.Add("Sum for recursion", recursion);
-            results.Add("Sum for ForLoop", TotalForLoop);
+            try
+            {
+                //sum for While Loop
+                while (index < intArray.Count)
+                {
+                    sumUsingWhileLoop += intArray[index];
+                    index = index + 1;
+                }
+
+                //sum for ForLoop
+                foreach (var item in intArray)
+                {
+                    TotalForLoop = TotalForLoop + item;
+
+                }
+                //sum for recursion
+                int recursion = web.SumRecurssion(intArray, 0, intArray.Count);
+
+               
+                results.Add("Sum for WhileLoop", sumUsingWhileLoop);
+                results.Add("Sum for recursion", recursion);
+                results.Add("Sum for ForLoop", TotalForLoop);
+            }
+            catch(Exception ex)
+            {    
+                web.WriteLog(ex, MethodBase.GetCurrentMethod().Name);
+                throw ex;  
+            }
             return results;
             //Display results on a string
             //  return string.Format("Sum for WhileLoop {0} : Sum for recursion {1} : Sum for ForLoop {2}", sumUsingWhileLoop, recursion, TotalForLoop);
@@ -90,17 +108,30 @@ namespace WebApI.Controllers
         public List<string> Post([FromBody] ListToCombine data)
         {
             var ArrayResults = new List<string>();
-
-            var FirstArray = data.list[0].ToList();
-            var SecondArray = data.list[1].Split(',').ToList();
-            var newList = SecondArray.Join(FirstArray, s => SecondArray.IndexOf(s), i => FirstArray.IndexOf(i), (s, i) => new { First = s, Second = i }).ToList();
-
-            foreach (var item in newList)
+            if (data == null)
             {
-                ArrayResults.Add(item.First.ToString());
-                ArrayResults.Add(item.Second.ToString());
+                throw new NullReferenceException("List of arrays can't be null");
             }
+            if (data != null)
+            {
+                var FirstArray = data.list[0].ToList();
+                var SecondArray = data.list[1].Split(',').ToList();
+                try
+                {
+                    var newList = SecondArray.Join(FirstArray, s => SecondArray.IndexOf(s), i => FirstArray.IndexOf(i), (s, i) => new { First = s, Second = i }).ToList();
 
+                    foreach (var item in newList)
+                    {
+                        ArrayResults.Add(item.First.ToString());
+                        ArrayResults.Add(item.Second.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    web.WriteLog(ex, MethodBase.GetCurrentMethod().Name);
+                    throw ex;
+                }
+            }
             return ArrayResults;
         }
 
@@ -113,10 +144,18 @@ namespace WebApI.Controllers
         public List<int> Get()
         {
             var FibonacciList = new List<int>();
-            for (int i = 0; i < 100; i++)
+            try
             {
-                FibonacciList.Add(web.FibonacciSum(i));
+                for (int i = 0; i < 100; i++)
+                {
+                    FibonacciList.Add(web.FibonacciSum(i));
 
+                }
+            }
+            catch (Exception ex)
+            {
+                web.WriteLog(ex, MethodBase.GetCurrentMethod().Name);
+                throw ex;
             }
 
             return FibonacciList;
